d8f818b002698bca7d6264738cfef29cb8799cad
souwang324 <33944965+souwang324@users.noreply.github.com>
2023-01-25 20:28:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TransformTest
+ TransformTest with CSharp Activiz x64 , Visual Studio 2017 Community, Windows10
---
+++ TransformTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kitware.VTK;
+
+namespace TransformTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransformTest1();
+        }
+        public static void TransformTest1()
+        {
+            vtkSTLReader pSTLReader = vtkSTLReader.New();
+            pSTLReader.SetFileName("../../../../res/cow.stl");
+            pSTLReader.Update();
+
+            vtkTransform pTransform = vtkTransform.New();
+            pTransform.Translate(10.0, 0.0, 0.0);
+            pTransform.RotateWXYZ(30, 0.0, 1.0, 0.0);
+
+            vtkTransformPolyDataFilter pTransformFilter = vtkTransformPolyDataFilter.New();
+            pTransformFilter.SetInputConnection(pSTLReader.GetOutputPort());
+            pTransformFilter.SetTransform(pTransform);
+            pTransformFilter.Update();
+
+            vtkPolyDataMapper mapper = vtkPolyDataMapper.New();
+            mapper.SetInputConnection(pTransformFilter.GetOutputPort());
+
+            vtkActor actor = vtkActor.New();
+            actor.SetMapper(mapper);
+
+            vtkRenderer renderer = vtkRenderer.New();
+            renderer.AddActor(actor);
+            renderer.SetBackground(.1, .2, .3);
+            renderer.ResetCamera();
+
+            vtkRenderWindow renderWin = vtkRenderWindow.New();
+            renderWin.AddRenderer(renderer);
+
+            vtkRenderWindowInteractor interactor = vtkRenderWindowInteractor.New();
+            interactor.SetRenderWindow(renderWin);
+
+            renderWin.Render();
+            interactor.Start();
+
+        }
+    }
+}
